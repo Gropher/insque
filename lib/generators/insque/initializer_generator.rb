997ec57307168f0b6b01c6364945a887a1f914a7
gropher <grophen@gmail.com>
2012-07-03 13:37:01 +0400

    Initializer generator updated

---
+++ lib/generators/insque/initializer_generator.rb
@@ -0,0 +1,18 @@
+module Insque
+  module Generators
+    class InitializerGenerator < ::Rails::Generators::Base
+      argument :sender_name, :type => :string
+      
+      desc 'Create a sample Insque initializer and redis config file'
+      
+      def self.source_root
+        @source_root ||= File.join(File.dirname(__FILE__), 'templates')
+      end
+
+      def create_initializer
+        template 'insque.rb', 'config/initializers/insque.erb'
+        template 'redis.yml', 'config/redis.yml'
+      end
+    end
+  end
+end
