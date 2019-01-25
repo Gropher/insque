bbf0d45bb321d854d2de799b806a7e39aef3228d
gropher <grophen@gmail.com>
2019-01-25 23:20:17 +0300

    Insque::JsonLogger fixed

--- lib/insque/json_logger.rb
+++ lib/insque/json_logger.rb
@@ -1,16 +1,11 @@
 module Insque
   class JsonLogger < ::Logger
     attr_accessor :additional_fields
-    def initialize(logdev, shift_age = 0, shift_size = 1048576, level: DEBUG,
-                 progname: nil, formatter: nil, datetime_format: nil,
-shift_period_suffix: '%Y%m%d')
-      super
-      @default_formatter = JsonFormatter.new
-      @additional_fields = {}
-    end
 
     def format_message(severity, datetime, progname, msg)
-      (@formatter || @default_formatter).call(severity, datetime, progname, msg, additional_fields)
+      @json_formatter ||= JsonFormatter.new
+      @additional_fields ||= {}
+      (@formatter || @json_formatter).call(severity, datetime, progname, msg, @additional_fields)
     end
 
     private
